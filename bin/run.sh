666601b72059d15d1c68a99130c348a0bda7bb2d
Takeru Sato <type.in.type@gmail.com>
2018-03-08 00:12:38 +0900

    Add Dockerfile and locust runner

---
+++ bin/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOCUST_OPTS="-f $SCENARIO_FILE --host=$TARGET_URL"
+LOCUST_MODE=${LOCUST_MODE:-standalone}
+
+if [ "$LOCUST_MODE" = "master" ]; then
+  LOCUST_OPTS="$LOCUST_OPTS --master"
+elif [ "$LOCUST_MODE" = "slave" ]; then
+  LOCUST_OPTS="$LOCUST_OPTS --slave --master-host=$MASTER_HOST"
+fi
+
+exec /usr/local/bin/locust $LOCUST_OPTS
